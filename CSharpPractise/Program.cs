dfb9b680ab893df1de32e66bad659ad252572f5c
Stuart Stephenson <stuart.stephenson@muddyboots.com>
2018-06-07 14:39:51 +0100

    Add project files.

---
+++ CSharpPractise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpPractise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var isValidEntry = false;
+            string word = "";
+
+            while (!isValidEntry)
+            {
+                Console.WriteLine("Please enter a word to see if it is a palindrome.");
+                word = Console.ReadLine();
+            }
+
+            if (IsPalindrome(word))
+            {
+                Console.WriteLine("The word you have entered is a palindrome");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("The word you have entered is not a palindrome");
+                Console.ReadLine();
+            }
+        }
+
+        public static bool IsPalindrome(string word)
+        {
+            var charList = new List<char>(word);
+            var reverseList = new List<char>();
+
+            for (var x = charList.Count - 1; x >= 0; x--)
+            {
+                reverseList.Add(charList[x]);           
+            }
+            
+            return (reverseList.SequenceEqual(charList)) ? true : false;
+        }
+    }
+}
---
+++ NuGetExampleWorkshop/NuGetWorkShop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetExampleWorkshop
+{
+    public static class NuGetWorkShop
+    {
+        public static String Example { get; set; }
+    }
+}
